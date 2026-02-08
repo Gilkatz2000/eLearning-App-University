775f8660cc7400b47343fbb3906c39f6b2e0ccb7
Gil Katz <sonexaai@gmail.com>
2026-02-08 19:06:35 +0200

    Now it is possible to register successfully.

--- accounts/views.py
+++ accounts/views.py
@@ -4,14 +4,13 @@ from django.shortcuts import render, redirect
 
 from .forms import RegisterForm, LoginForm
 
-
 def register_view(request):
     if request.method == "POST":
         form = RegisterForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("home")
+            return redirect("course_list")
     else:
         form = RegisterForm()
     return render(request, "accounts/register.html", {"form": form})
@@ -22,7 +21,7 @@ def login_view(request):
         form = LoginForm(request, data=request.POST)
         if form.is_valid():
             login(request, form.get_user())
-            return redirect("home")
+            return redirect("course_list")
     else:
         form = LoginForm()
     return render(request, "accounts/login.html", {"form": form})
