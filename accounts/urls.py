5b0ec189ed558e24492e7767aa3ad919ef201df1
Gil Katz <sonexaai@gmail.com>
2026-02-09 17:34:29 +0200

    feat: add user home pages with status updates

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
+
+    path("users/<str:username>/", views.user_home, name="user_home"),
+    path("status/post/", views.post_status_update, name="post_status_update"),
 ]
